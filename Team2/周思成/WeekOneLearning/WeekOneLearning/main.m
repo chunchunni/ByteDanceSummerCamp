ca22b06d6753b784673b78fb899cbe674e3a3a2f
ZhouScccc <robin.zhou0412@gmail.com>
2021-06-15 21:45:44 -0700

    testpush

---
+++ Team2/周思成/WeekOneLearning/WeekOneLearning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeekOneLearning
+//
+//  Created by zsc on 2021/6/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
