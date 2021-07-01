9fc5a86975a3052836f3851559f5bcad0bd3bc5f
Ars-champion <85595905+Ars-champion@users.noreply.github.com>
2021-07-01 18:55:29 +0800

    Fraction_Calculator

---
+++ Team3/贾皓珑/Calculator.h
@@ -0,0 +1,16 @@
+
+#import <UIKit/UIKit.h>
+#import "Fraction.h"
+
+@interface Calculator:NSObject
+
+
+@property (strong, nonatomic) Fraction *operand1;
+@property (strong, nonatomic) Fraction *operand2;
+@property (strong, nonatomic) Fraction *accumulator;
+
+-(Fraction *) performOperation: (char) op;
+-(void) clear;
+
+@end
+
