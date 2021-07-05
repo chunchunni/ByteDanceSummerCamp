4a7159deb9428ddee2010f2d96c805b21045137a
Ars-champion <85595905+Ars-champion@users.noreply.github.com>
2021-07-05 16:50:28 +0800

    Add files via upload

---
+++ Team3/贾皓珑/Fraction_Calculator/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Fraction_Calculator_2
+//
+//  Created by Admin on 2021/7/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Team3/贾皓珑/Fraction_Calculator/Fraction.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+@interface Fraction : NSObject
+
+@property int numerator,denominator;
+
+-(void)             print; -(void)             setTo: (int) n over: (int) d; -(Fraction *)       add: (Fraction *) f;
+-(Fraction *)       subtract: (Fraction *) f;
+-(Fraction *)       multiply: (Fraction *) f; -(Fraction *)       divide: (Fraction *) f; -(void)             reduce;
+-(double)           convertToNum;
+-(NSString *)       convertToString;
+
+@end
+
