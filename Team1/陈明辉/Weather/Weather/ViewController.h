80c983cb388b91e6fc1d4fdc80a845c81dbf0151
conanhui1 <guyehu@qq.com>
2021-07-29 01:23:11 -0700

    学习总结

--- Team1/陈明辉/Weather/Weather/ViewController.h
+++ Team1/陈明辉/Weather/Weather/ViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "myWeather.h"
 #import "AFNetworking.h"
+#import <CoreLocation/CoreLocation.h>
 @interface ViewController : UIViewController
 
 @property (weak,nonatomic) IBOutlet UILabel *l1;
@@ -19,10 +20,24 @@
 @property (weak,nonatomic) IBOutlet UILabel *next2_wea;
 @property (weak,nonatomic) IBOutlet UILabel *next2_temp;
 @property (weak,nonatomic) IBOutlet UILabel *date;
+//定位
 
 @property myWeather *weather;
+
+    //定位
+@property(strong,nonatomic) CLLocationManager *_locationManager;//用于获取位置
+@property(strong,nonatomic) CLLocation *_checkLocation;//用于保存位置信息
+    
+@property    double latitude;//经度
+@property    double longitude;//维度
+    
+
 - (void) processdata:(NSDictionary *)dict;
 - (void)loadData;
 - (void) bt:(UIButton *)sender;
+- (void)setupLocationManager;
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray*)locations;
+- (void) showLocation;
+
 @end
 
--- Team1/陈明辉/Weather/Weather/ViewController.m
+++ Team1/陈明辉/Weather/Weather/ViewController.m
@@ -18,13 +18,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self loadData];
+    [self showLocation];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 //加载网络数据
 -(void)loadData
@@ -66,13 +63,82 @@
             }] resume];
     UIImage *image = [UIImage imageNamed:@"bg.jpg"];
     
-    self.view.layer.contents = (id) image.CGImage;    // 如果需要背景透明加上下面这句
+    self.view.layer.contents = (id) image.CGImage;
     
     self.view.layer.backgroundColor = [UIColor clearColor].CGColor;
+    
 }
 - (void)processdata:(NSDictionary *)dict:(NSDictionary *)dict
 {
     
 }
+
+/*- (void) setupLocationManager{
+    _latitude = LATITUDE_DEFAULT;//设置默认的值
+    _longitude =LONGITUDE_DEFAULT;
+    _locationManager = [[CLLocationManageralloc]init];
+    
+    if ([CLLocationManagerlocationServicesEnabled]) {
+        NSLog(@"开始定位");
+        _locationManager.delegate = self;
+        // distanceFilter是距离过滤器，为了减少对定位装置的轮询次数，位置的改变不会每次都去通知委托，而是在移动了足够的距离时才通知委托程序
+        //它的单位是米，这里设置为至少移动1000再通知委托处理更新;
+        _locationManager.distanceFilter = 200.0;
+        // kCLLocationAccuracyBest:设备使用电池供电时候最高的精度
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        
+        
+        //ios8+以上要授权，并且在plist文件中添加NSLocationWhenInUseUsageDescription，NSLocationAlwaysUsageDescription，值可以为空
+        if (IOS_VERSION >=8.0) {//ios8+，不加这个则不会弹框
+            [_locationManagerrequestWhenInUseAuthorization];//使用中授权
+            [_locationManagerrequestAlwaysAuthorization];
+        }
+        [_locationManagerstartUpdatingLocation];
+    }else{
+        NSLog(@"定位失败，请确定是否开启定位功能");
+    }
+}
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray*)locations{
+    NSLog(@"didUpdateToLocation+++");
+    //此处locations存储了持续更新的位置坐标值，取最后一个值为最新位置，如果不想让其持续更新位置，则在此方法中获取到一个值之后让locationManager stopUpdatingLocation
+    CLLocation *cl = [locationslastObject];
+    _latitude = cl.coordinate.latitude;
+    _longitude = cl.coordinate.longitude;
+    NSLog(@"纬度--%f",_latitude);
+    NSLog(@"经度--%f",_longitude	);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"定位失败");
+}
+ */
+- (void) showLocation{
+    //CLLocation *current_location =;
+    
+    float latitude = 30.911030000;
+    float longitude = 103.4135540000;
+    // 创建经纬度
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+    //创建一个译码器
+    CLGeocoder *cLGeocoder = [[CLGeocoder alloc] init];
+    [cLGeocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *place = [placemarks objectAtIndex:0];
+        // 位置名
+        NSLog(@"name,%@",place.name);
+        // 街道
+        NSLog(@"thoroughfare,%@",place.thoroughfare);
+        // 子街道
+        NSLog(@"subThoroughfare,%@",place.subThoroughfare);
+        // 市
+        NSLog(@"locality,%@",place.locality);
+        // 区
+        NSLog(@"subLocality,%@",place.subLocality);
+        // 国家
+        NSLog(@"country,%@",place.country);
+    }];
+    
+    
+}
+ 
 @end
 
